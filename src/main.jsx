46bb7a29d43aceeac803e886b7fa47a0dff6c3cd
yairhdz24 <yairhdz107@gmail.com>
2024-01-05 00:32:47 -0600

    rutas nuevas agregadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { HomePage } from './pages/HomePage.jsx'
-import { GeneradorTarjetas } from './components/Generador.jsx';
+import { BinCheck } from './pages/BinCheck.jsx'
+import { HomePage1 } from './pages/HomePage1.jsx'
+import { GeneradorTarjetas } from './pages/Generador.jsx';
 import { MiAMOR } from './components/Bin_Data.jsx';
+import { ToastContainer } from 'react-toastify';
 
 
 import './index.css'
@@ -12,12 +14,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage1 />} />
         <Route path="/GeneradorTarjetas" element={<GeneradorTarjetas />} />
+        <Route path="/BinChecker" element={<BinCheck />} />
         <Route path="/Emilly<3" element={<MiAMOR/>} />
         {/* <Route path="/clientes" element={<Clientes />} /> */}
         {/* <Route path="/HistorialPedidos" element={<HistorialPedidos />} /> */}
       </Routes>
+      <ToastContainer />
     </Router>
   </React.StrictMode>,
 );
